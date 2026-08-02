3f3eecb06c87cda386bb8fffc9c56180f98450af
LexerPatty <pushpendudey44@gmail.com>
2026-08-02 13:30:48 +0530

    tools related for agents

---
+++ main.py
@@ -0,0 +1,7 @@
+from tools.tools import web_search , scrape_url
+
+# result = web_search.invoke("What is the latest news on AI?")
+# print(result)
+
+# scraped_content = scrape_url.invoke("https://tech.yahoo.com/ai")
+# print(scraped_content)
