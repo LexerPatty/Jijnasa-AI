970324236321052b329c7c40fdf6dfa4ee7898c5
LexerPatty <pushpendudey44@gmail.com>
2026-08-02 15:53:57 +0530

    Complete the level 1

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from tools.tools import web_search , scrape_url
+from pipeline import run_research_pipeline
 
 # result = web_search.invoke("What is the latest news on AI?")
 # print(result)
 
 # scraped_content = scrape_url.invoke("https://tech.yahoo.com/ai")
 # print(scraped_content)
+
+
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
